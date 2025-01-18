57075c17e69c74372f247d558a39897cce0d40b3
AsharASAD <157423144+AsharASAD@users.noreply.github.com>
2025-01-18 15:50:36 -0500

    Week 2

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//IIFE - Immediately Invoked Function Expression
+//AKA - Anonymous Self-Executing Function
+
+(function () {
+
+    function Start()
+    {
+        console.log("App Started!");
+    }
+    window.addEventListener("load", Start);
+})();
